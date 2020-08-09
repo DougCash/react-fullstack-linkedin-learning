a809c1232a124f6691318efe2f2e10bea82a7e55
Doug Cash <dcash@Dougs-MacBook-Pro.local>
2020-08-09 11:44:47 -0500

    Connected to Mongo, introduce React Hook

--- my-blog/src/pages/articlePage.js
+++ my-blog/src/pages/articlePage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import articleContent from './articleContent'
 import ArticlesList from '../components/articlesList'
 import NotFoundPage from './notFoundPage';
@@ -8,13 +8,20 @@ const ArticlePage = ({ match }) => {
     const name = match.params.name;
     const article = articleContent.find(article => article.name === name);
 
+    const [articleInfo, setArticleInfo] = useState({upvotes: 0, comments: []});
+
+    useEffect(() => {
+        setArticleInfo({upvotes: 3});
+    })
+
     if (!article) return <NotFoundPage />
 
-    const otherArticles = articleContent.filter(article => article.name !== name)
+    const otherArticles = articleContent.filter(article => article.name !== name);
 
     return (
         <React.Fragment>
             <h1>{article.title}</h1>
+            <p>This article has been upvoted {articleInfo.upvotes} times</p>
             {article.content.map((paragraph, key) => (
                 <p key={key}>{paragraph}</p>
             ))}
